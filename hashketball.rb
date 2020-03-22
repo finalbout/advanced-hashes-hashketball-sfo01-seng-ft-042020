d00768a2574fcfcb9b1bf6a26b01afef17aa1e0f
John Tom Jr <johntjr.22@gmail.com>
2020-03-22 00:22:07 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -145,6 +145,8 @@ if team_info[:team_name] == input
   team_info.each do |key,value|
   if key == :players 
     value.each do |player|
+      output.push(player[:number])
+    end
       
       
       end
